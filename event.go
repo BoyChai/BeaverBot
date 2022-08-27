700de769df25d7359d3bfacba43ceb149e9f6e0a
BoyChai <1972567225@qq.com>
2022-08-27 16:04:23 +0800

    Signature authentication, multi bot ID judgment

--- event.go
+++ event.go
@@ -1,6 +1,21 @@
 package BeaverBot
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Event 整个事件信息
 type Event struct {
+	header struct {
+		timestamp int64
+		sign      string
+	}
+	HandleID       int
 	ConversationId string `json:"conversationId"`
 	AtUsers        []struct {
 		DingtalkId string `json:"dingtalkId"`
@@ -24,4 +39,57 @@ type Event struct {
 		Content string `json:"content"`
 	} `json:"text"`
 	Msgtype string `json:"msgtype"`
+}
+
+// 消息签名信息
+type whoSign struct {
+	timestamp int64
+	sign      string
+}
+
+func worker(pool chan whoSign, wg *sync.WaitGroup, id *int) {
+	for signInfo := range pool {
+		for i, handle := range AllHandle {
+			secStr := fmt.Sprintf("%d\n%s", signInfo.timestamp, handle.AppSecret)
+			hmac256 := hmac.New(sha256.New, []byte(handle.AppSecret))
+			hmac256.Write([]byte(secStr))
+			result := hmac256.Sum(nil)
+			sign := base64.StdEncoding.EncodeToString(result)
+			if sign == signInfo.sign {
+				*id = i + 1
+			}
+		}
+		wg.Done()
+	}
+
+}
+
+//func (e *Event)sign(t int64, secret string) string {
+//	secStr := fmt.Sprintf("%d\n%s", t, secret)
+//	hmac256 := hmac.New(sha256.New, []byte(secret))
+//	hmac256.Write([]byte(secStr))
+//	result := hmac256.Sum(nil)
+//	return base64.StdEncoding.EncodeToString(result)
+//}
+func (e *Event) explain(pool *chan whoSign, wg *sync.WaitGroup, id *int) {
+	// 获取当前时间戳(毫秒)
+	now := time.Now()
+	nowTime := now.UnixNano() / 1e6
+	// 时间判断是否合法
+	if (e.header.timestamp-nowTime)/3600000 >= 1 {
+		return
+	}
+	wg.Add(1)
+
+	*pool <- whoSign{
+		timestamp: e.header.timestamp,
+		sign:      e.header.sign,
+	}
+	wg.Wait()
+	if *id == 0 {
+		fmt.Println("判断失败")
+	}
+	e.HandleID = *id
+	*id = 0
+	fmt.Println("签名有效,机器人ID为:", e.HandleID)
 }
--- gin.go
+++ gin.go
@@ -6,30 +6,52 @@ import (
 	"github.com/gin-gonic/gin"
 	"io"
 	"io/ioutil"
+	"strconv"
+	"sync"
 )
 
 func RunBeaverBot(port string, e *Event) {
+	// bot判断缓存池(判断任务是那个机器人来处理的缓存池,4是数量)
+	var wg sync.WaitGroup
+	var id int
+	pool := make(chan whoSign, 4)
+	for i := 0; i < cap(pool); i++ {
+		go worker(pool, &wg, &id)
+	}
+	// gin
 	g := gin.New()
 	var init Event
 	g.POST("/", func(c *gin.Context) {
-		dataReader := c.Request.Body
-		bodyData, err := ioutil.ReadAll(dataReader)
+		*e = init
+		// body处理
+		dataBody := c.Request.Body
+		bodyData, err := ioutil.ReadAll(dataBody)
 		defer func(dataReader io.ReadCloser) {
 			err := dataReader.Close()
 			if err != nil {
 				fmt.Println(err)
 			}
-		}(dataReader)
-
+		}(dataBody)
 		if err != nil {
 			fmt.Println(err)
 		}
-		*e = init
 		err = json.Unmarshal(bodyData, &e)
 		if err != nil {
 			fmt.Println("command parsing error,please feedback to the developer.error:", err)
 		}
-		fmt.Println(e)
+
+		// header处理
+		timestamp, err := strconv.ParseInt(c.Request.Header.Get("timestamp"), 10, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		e.header.timestamp = timestamp
+		e.header.sign = c.Request.Header.Get("sign")
+		defer c.Request.Header.Clone()
+
+		// 执行事件处理器
+		e.explain(&pool, &wg, &id)
+
 	})
 	err := g.Run(port)
 	if err != nil {
